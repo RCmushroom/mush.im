ceda648930e903ae7ab724a0007522f461691e9e
Wany <sung@wany.io>
2024-08-05 18:42:14 +0900

    워크플로 수정

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 session=github-actions-mush-im
 tmux has-session -t $session || tmux new-session -d -s $session
+tmux set-option -t $session -p remain-on-exit
 tmux send-keys -t $session C-c
 tmux send-keys -t $session "bash" C-m
 tmux send-keys -t $session "cd $PWD" C-m
